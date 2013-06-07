7aeff1c0ce22f3167ec7fa6287d6b60dbce5aa69
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-07 11:33:13 -0500

    card and deck migrations

--- db/migrate/20130607113001_create_cards.rb
+++ db/migrate/20130607113001_create_cards.rb
@@ -1,4 +1,11 @@
 class CreateCards < ActiveRecord::Migration
   def change
+    create_table :cards do |t|
+      t.string :prompt
+      t.string :solution
+      t.integer :deck_id
+
+      t.timestamps
+    end
   end
 end
