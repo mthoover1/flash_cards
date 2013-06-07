f7464fc4471962f27a8fedf64dc360ff1c91adb3
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-07 16:57:59 -0500

    we've been busy

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -18,17 +18,29 @@ end
 
 get '/decks' do
   @decks = Deck.all
+  @round = Round.create
   erb :decks
 end
 
-get '/decks/:deck_id' do
-  @deck = Deck.find_by_id(params[:deck_id])
-  @round = Round.create
-  @round.deck = @deck
-  erb :round
+get '/rounds/:deck_id/:round_id' do
+  @deck = Deck.find(params[:deck_id])
+  @round = Round.find(params[:round_id])
+  guessed_card_ids = Guess.all.map(&:card_id)
+  deck_card_ids = @deck.cards.all.map(&:id)
+  remaining_cards = deck_card_ids - guessed_card_ids
+  if remaining_cards.empty?
+    redirect to '/rounds/' + @round.id.to_s + '/results'
+  else
+    @card = Card.find(remaining_cards.sample)
+    erb :round_card
+  end
 end
 
-get '/rounds/:round_id' do
+# post '/rounds/:deck_id/:card_id' do
+#   @guess = Guess.create(params[:form])
+# end
+
+get '/rounds/:round_id/results' do
   @round = Round.find_by_id(params[:round_id])
   @guesses = Guess.find_all_by_round_id(params[:found_id])
   erb :round_results
