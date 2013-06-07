293c1c72481bcf42f55e9106fdd2eafc71dbf256
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-07 17:20:29 -0500

    update score method in helper

--- app/helpers/score.rb
+++ app/helpers/score.rb
@@ -1,21 +1,11 @@
 def score
-  if self.deck_id == 1
-    weighted_score   # => User can earn points with a close guess
-  else
-    standard_score
-  end
-end
-
-
-def standard_score
   correct = self.sum("correctness")   # OR self.guesses.where(correctness: 1).count
-  total = self.guesses.count
+  
+  if self.deck_id == 1
+    total = self.guesses.count * 5
+  else
+    total = self.guesses.count
+  end
 
   (correct.to_f / total.to_f).round(1)
-end
-
-
-
-def weighted_score
-  
 end
