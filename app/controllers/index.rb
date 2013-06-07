797fc64bfba8df35845d6612bca81c4982662feb
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-07 13:36:25 -0500

    Adds deck and log_in view alongside index controller

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,22 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/login' do
+  erb :log_in
+end
+
+post '/login' do
+  @user = User.authenticate(params[form])
+  if @user
+    redirect to '/decks'
+  else
+    erb :index
+  end
+end
+
+get '/decks' do
+  @decks = Decks.all
+  erb :decks
+end
+
