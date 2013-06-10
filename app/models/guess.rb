4f3dc24bea5691a5c9e3d1905830316572ea93b1
Matthew Hoover <mthoover1@gmail.com>
2013-06-09 22:46:00 -0500

    refactor logic for proximity of guess (movies)

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -14,12 +14,12 @@ class Guess < ActiveRecord::Base
     unless @deck.id == 1
       self.correctness = 1 if self.response.downcase.gsub(".", "") == Card.find(card_id).solution.downcase.gsub(".", "")
     else
-      proximity = self.response.to_i - Card.find(card_id).solution.to_i
+      proximity = (self.response.to_i - Card.find(card_id).solution.to_i).abs
 
       self.correctness = 5 if proximity == 0
-      self.correctness = 3 if proximity == 1 || proximity == -1
-      self.correctness = 2 if proximity == 2 || proximity == -2
-      self.correctness = 1 if proximity == 3 || proximity == -3
+      self.correctness = 3 if proximity == 1
+      self.correctness = 2 if proximity == 2
+      self.correctness = 1 if proximity == 3
     end
   end
 
